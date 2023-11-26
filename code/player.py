e3fb2ba1a6e1ec0fe9388a744160cc66190db78f
Icy-ici <rwhee30@outlook.com>
2023-11-27 08:27:19 +1000

    m

--- code/player.py
+++ code/player.py
@@ -191,23 +191,22 @@ class Player(pygame.sprite.Sprite):
 
     def collision(self, direction):
         for sprite in self.collision_sprites.sprites():
-            if hasattr(sprite, 'hitbox'):
-                if sprite.hitbox.colliderect(self.hitbox):
-                    if direction == 'horizontal':
-                        if self.direction.x > 0:  # moving right
-                            self.hitbox.right = sprite.hitbox.left
-                        if self.direction.x < 0:  # moving left
-                            self.hitbox.left = sprite.hitbox.right
-                        self.rect.centerx = self.hitbox.centerx
-                        self.pos.x = self.hitbox.centerx
-
-                    if direction == 'vertical':
-                        if self.direction.y > 0:  # moving down
-                            self.hitbox.bottom = sprite.hitbox.top
-                        if self.direction.y < 0:  # moving up
-                            self.hitbox.top = sprite.hitbox.bottom
-                        self.rect.centery = self.hitbox.centery
-                        self.pos.y = self.hitbox.centery
+            if hasattr(sprite, 'hitbox') and sprite.hitbox.colliderect(self.hitbox):
+                if direction == 'horizontal':
+                    if self.direction.x > 0:  # moving right
+                        self.hitbox.right = sprite.hitbox.left
+                    if self.direction.x < 0:  # moving left
+                        self.hitbox.left = sprite.hitbox.right
+                    self.rect.centerx = self.hitbox.centerx
+                    self.pos.x = self.hitbox.centerx
+            
+                elif direction == 'vertical':
+                    if self.direction.y > 0:  # moving down
+                        self.hitbox.bottom = sprite.hitbox.top
+                    if self.direction.y < 0:  # moving up
+                        self.hitbox.top = sprite.hitbox.bottom
+                    self.rect.centery = self.hitbox.centery
+                    self.pos.y = self.hitbox.centery
 
     def move(self, dt):
         # normalizing a vector
